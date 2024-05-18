23fbee3a8dd91546d12f72f6a95c168f0c752bde
kubatovtamas <kuba@secretsaucepartners.com>
2024-05-18 16:56:54 +0200

    Updates

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -11,27 +11,11 @@ return {
 		vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 		vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
 
-
         -- For using Treesitter as the backend
 		require("ufo").setup({
 			provider_selector = function(bufnr, filetype, buftype)
 				return { "treesitter", "indent" }
 			end,
 		})
-
-        -- For using LSP as the backend
-		-- local capabilities = vim.lsp.protocol.make_client_capabilities()
-		-- capabilities.textDocument.foldingRange = {
-		-- 	dynamicRegistration = false,
-		-- 	lineFoldingOnly = true,
-		-- }
-		-- local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
-		-- for _, ls in ipairs(language_servers) do
-		-- 	require("lspconfig")[ls].setup({
-		-- 		capabilities = capabilities,
-		-- 		-- you can add other fields for setting up lsp server in this table
-		-- 	})
-		-- end
-		-- require("ufo").setup()
 	end,
 }
