65124d44664da488345d547d92a94d3af4968012
Kubatov Tamás <kubatovtamas01@gmail.com>
2024-09-25 08:54:01 +0200

    Resolve leap and nvim-surround conflict

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,10 +1,22 @@
 return {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end
+	"kylechui/nvim-surround",
+	version = "*", -- Use for stability; omit to use `main` branch for the latest features
+	event = "VeryLazy",
+	config = function()
+        local nvim_surround = require("nvim-surround")
+        nvim_surround.setup({
+			keymaps = {
+				insert = "<leader><C-g>s",
+				insert_line = "<leader><C-g>S",
+				normal = "<leader>gs",
+				normal_cur = "<leader>gS",
+				normal_line = "<leader>gss",
+				normal_cur_line = "<leader>gSS",
+				visual = "<leader>gs",
+				visual_line = "<leader>gS",
+				delete = "<leader>gsd",
+				change = "<leader>gsc",
+			},
+		})
+	end,
 }
