3446b98252c08ab5c36e564f2770d1e1b583de93
Kubatov Tamás <45062503+kubatovtamas@users.noreply.github.com>
2026-03-15 12:26:41 +0100

    nvim.leap update

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,6 +1,28 @@
 return {
-	"ggandor/leap.nvim",
+	url = "https://codeberg.org/andyg/leap.nvim",
 	config = function()
-		require("leap").create_default_mappings()
+		local apply_backdrop = function()
+			if vim.g.colors_name and vim.g.colors_name:match("^catppuccin") then
+				require("leap.user").set_backdrop_highlight("Comment")
+			end
+		end
+
+		vim.api.nvim_create_autocmd("ColorScheme", {
+			group = vim.api.nvim_create_augroup("LeapBackdrop", {}),
+			callback = apply_backdrop,
+		})
+		apply_backdrop()
+
+        require('leap.user').set_repeat_keys('<enter>', '<backspace>')
+
+		vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward)")
+		vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
+		vim.keymap.set({ "x", "o" }, "R", function()
+			require("leap.treesitter").select({
+				opts = require("leap.user").with_traversal_keys("R", "r"),
+			})
+		end)
+
+		require("leap").opts.equivalence_classes = { " \t\r\n", "([{", ")]}", "'\"`" }
 	end,
 }
